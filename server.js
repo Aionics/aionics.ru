09dc60390a9651ea2f6297debc12e511694bc4f0
Artem <aionics@list.ru>
2016-09-26 19:26:18 +0500

    start scripts

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var server = express();
 var path = require('path');
-var PORT = 8084;
 
 server.use('/', express.static('./www'));
 server.get('/*', function(req, res){
 	res.sendFile('index.html', { root: __dirname + '/www' });
 });
 
-server.listen(PORT, function() {
-	console.log('listening ' + PORT);
+server.set('port', process.env.PORT || 8084);
+server.listen(server.get('port'), function() {
+	console.log('listening ' + server.get('port'));
 });
