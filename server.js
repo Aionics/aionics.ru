78b95fd862567766bc50c01bfbfcbc7873514569
Artem <aionics@list.ru>
2016-09-26 20:42:24 +0500

    test

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ server.get('/*', function(req, res){
 	res.sendFile('index.html', { root: __dirname + '/www' });
 });
 
+
 server.set('port', process.env.PORT || 8084);
 server.listen(server.get('port'), function() {
 	console.log('listening ' + server.get('port'));
