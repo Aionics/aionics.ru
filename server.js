2a9fbe8ff94adca233507a721f163c53084e3539
Artem <aionics@list.ru>
2016-09-26 21:19:35 +0500

    config added to package json

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ var server = express();
 var path = require('path');
 
 server.use('/', express.static('./www'));
-server.get('/*', function(req, res){
+
+server.get('/manage', function(req, res) {
+	
+})
+server.get('/', function(req, res){
 	res.sendFile('index.html', { root: __dirname + '/www' });
 });
 
-server.set('port', process.env.PORT || 8084);
+server.set('port', process.env.PORT || process.env.npm_package_config_port);
 server.listen(server.get('port'), function() {
 	console.log('listening ' + server.get('port'));
 });
